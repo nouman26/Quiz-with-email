b7b517da9ed9c9e5485b03d69857014deaf9a10e
nouman26 <60352449+nouman26@users.noreply.github.com>
2020-10-21 17:47:50 +0500

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/",(req,res)=>{
 });
 
 app.post("/",(req,res)=>{
-    us=["18CS26@quest.edu.pk",req.body.email]
+    
     const output = `
     <h3>Contact Details</h3>
     <ul>  
@@ -40,10 +40,12 @@ app.post("/",(req,res)=>{
     }
   });
 
+  var maillist=["18CS26@quest.edu.pk",req.body.email]
+
   // setup email data with unicode symbols
   let mailOptions = {
       from: 'noumanarain0@gmail.com', // sender address
-      to: us,// list of receivers
+      to: maillist,// list of receivers
       subject: 'Node Contact Request', // Subject line
       text: 'Quiz Result', // plain text body
       html: output // html body
