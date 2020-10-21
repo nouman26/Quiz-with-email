850afe73a754fbeaef59cffef77df4051bab1371
nouman26 <60352449+nouman26@users.noreply.github.com>
2020-10-21 16:14:04 +0500

    1st commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require("express");
+const nodemailer=require("nodemailer");
+const path=require("path");
+const { body } = require("express-validator");
 const app=express();
-const path = require('path')
 
 let port=process.env.PORT || 5000;
 
@@ -15,8 +17,50 @@ app.get("/",(req,res)=>{
     res.render('index')
 });
 
-app.post('/', function (req, res) {  
-    res.send(req.body.Result+' Submitted Successfully!');
+app.post("/",(req,res)=>{
+    us="18CS26@quest.edu.pk"
+    const output = `
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Roll No: ${req.body.roll}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Result</h3>
+    <p>${req.body.Result}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    auth: {
+        user: 'noumanarain0@gmail.com', // generated ethereal user
+        pass: '0302210676'  // generated ethereal password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: 'noumanarain0@gmail.com', // sender address
+      to: req.body.email,us,// list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Quiz Result', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  });
+
+    res.render('result')
 });
+
 
 app.listen(port,()=>console.log("server running on port http://localhost:$(5000)"))
