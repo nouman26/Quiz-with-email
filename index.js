41f2ceda6d5ed9513b2802b66f6ddc29265b2922
nouman26 <60352449+nouman26@users.noreply.github.com>
2020-10-21 11:36:55 +0500

    1st Commit

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const express=require("express");
 const app=express();
 const path = require('path')
 
-let port=process.env.PORT || 8080;
+let port=process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'twig');
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
+    res.render('index')
 });
 
 app.post('/', function (req, res) {  
     res.send(req.body.Result+' Submitted Successfully!');
 });
 
-app.listen(port,()=>console.log("server running on port http://localhost:$(3000)"))
+app.listen(port,()=>console.log("server running on port http://localhost:$(5000)"))
